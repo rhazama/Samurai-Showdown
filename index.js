108650c433af4b592edbfe216ac5dec2436812ce
rhazama <rhazama01@gmail.com>
2022-08-10 15:04:14 -0700

    creating player and enemy, gravity in game

--- index.js
+++ index.js
@@ -6,32 +6,40 @@ canvas.height = 576;
 
 c.fillRect(0, 0, canvas.width, canvas.height);
 
+const gravity = 0.2;
+
 class Sprite {
   constructor({ position, velocity }) {
     this.position = position;
     this.velocity = velocity;
+    this.height = 150;
   }
 
   draw() {
     c.fillStyle = "red";
-    c.fillRect(this.position.x, this.position.y, 50, 150);
+    c.fillRect(this.position.x, this.position.y, 50, this.height);
   }
 
   update() {
     this.draw();
-    this.position.y += 10;
+    this.position.y += this.velocity.y;
+
+    if (this.position.y + this.height + this.velocity.y >= canvas.height) {
+      this.velocity.y = 0;
+    } else this.velocity.y += gravity;
+
   }
 }
 
 const player = new Sprite({
   position: {
     x: 0,
-    y: 0,
+    y: 10,
   },
   velocity: {
     x: 0,
     y: 0,
-  }
+  },
 });
 
 const enemy = new Sprite({
@@ -42,13 +50,15 @@ const enemy = new Sprite({
   velocity: {
     x: 0,
     y: 0,
-  }
+  },
 });
 
 console.log(player);
 
 function animate() {
   window.requestAnimationFrame(animate);
+  c.fillStyle = "black";
+  c.fillRect(0, 0, canvas.width, canvas.height);
   player.update();
   enemy.update();
 }
