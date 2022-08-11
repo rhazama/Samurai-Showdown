eb090256c4085b378536348756e69a100dad25ab
rhazama <rhazama01@gmail.com>
2022-08-10 22:55:04 -0700

    adding jump button

--- index.js
+++ index.js
@@ -22,6 +22,7 @@ class Sprite {
 
   update() {
     this.draw();
+    
     this.position.x += this.velocity.x;
     this.position.y += this.velocity.y;
 
@@ -62,7 +63,11 @@ const keys = {
   d: {
     pressed: false,
   },
+  w: {
+    pressed: false,
+  },
 };
+let lastKey;
 
 function animate() {
   window.requestAnimationFrame(animate);
@@ -71,9 +76,11 @@ function animate() {
   player.update();
   enemy.update();
 
-  if (keys.a.pressed) {
+  player.velocity.x = 0;
+
+  if (keys.a.pressed && lastKey === "a") {
     player.velocity.x = -1;
-  } else if (keys.d.pressed) {
+  } else if (keys.d.pressed && lastKey === "d") {
     player.velocity.x = 1;
   }
 }
@@ -84,9 +91,14 @@ window.addEventListener("keydown", (event) => {
   switch (event.key) {
     case "d":
       keys.d.pressed = true;
+      lastKey = "d";
       break;
     case "a":
       keys.a.pressed = true;
+      lastKey = "a";
+      break;
+    case "w":
+      player.velocity.y = -10;
       break;
   }
   console.log(event.key);
@@ -99,6 +111,10 @@ window.addEventListener("keyup", (event) => {
       break;
     case "a":
       keys.a.pressed = false;
+      break;
+    case "w":
+      keys.a.pressed = false;
+      lastKey = "w";
       break;
   }
   console.log(event.key);
