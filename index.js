cbdc2df6a1f0a6784974450ab47983d28790b879
rhazama <rhazama01@gmail.com>
2022-08-11 00:11:45 -0700

    adding movement to enemy

--- index.js
+++ index.js
@@ -13,6 +13,7 @@ class Sprite {
     this.position = position;
     this.velocity = velocity;
     this.height = 150;
+    this.lastKey;
   }
 
   draw() {
@@ -22,7 +23,7 @@ class Sprite {
 
   update() {
     this.draw();
-    
+
     this.position.x += this.velocity.x;
     this.position.y += this.velocity.y;
 
@@ -63,7 +64,10 @@ const keys = {
   d: {
     pressed: false,
   },
-  w: {
+  ArrowRight: {
+    pressed: false,
+  },
+  ArrowLeft: {
     pressed: false,
   },
 };
@@ -77,28 +81,50 @@ function animate() {
   enemy.update();
 
   player.velocity.x = 0;
+  enemy.velocity.x = 0;
 
-  if (keys.a.pressed && lastKey === "a") {
-    player.velocity.x = -1;
-  } else if (keys.d.pressed && lastKey === "d") {
-    player.velocity.x = 1;
+  // player movement
+  if (keys.a.pressed && player.lastKey === "a") {
+    player.velocity.x = -5;
+  } else if (keys.d.pressed && player.lastKey === "d") {
+    player.velocity.x = 5;
+  }
+
+  // enemy movement
+  if (keys.ArrowLeft.pressed && enemy.lastKey === "ArrowLeft") {
+    enemy.velocity.x = -5;
+  } else if (keys.ArrowRight.pressed && enemy.lastKey === "ArrowRight") {
+    enemy.velocity.x = 5;
   }
 }
 
 animate();
 
 window.addEventListener("keydown", (event) => {
+  console.log(event.Key)
   switch (event.key) {
     case "d":
       keys.d.pressed = true;
-      lastKey = "d";
+      player.lastKey = "d";
       break;
     case "a":
       keys.a.pressed = true;
-      lastKey = "a";
+      player.lastKey = "a";
       break;
     case "w":
       player.velocity.y = -10;
+      break;
+
+    case "ArrowRight":
+      keys.ArrowRight.pressed = true;
+      enemy.lastKey = "ArrowRight";
+      break;
+    case "ArrowLeft":
+      keys.ArrowLeft.pressed = true;
+      enemy.lastKey = "ArrowLeft";
+      break;
+    case "ArrowUp":
+      enemy.velocity.y = -10;
       break;
   }
   console.log(event.key);
@@ -112,10 +138,16 @@ window.addEventListener("keyup", (event) => {
     case "a":
       keys.a.pressed = false;
       break;
-    case "w":
-      keys.a.pressed = false;
-      lastKey = "w";
+  }
+
+  switch (event.key) {
+    case "ArrowRight":
+      keys.ArrowRight.pressed = false;
+      break;
+    case "ArrowLeft":
+      keys.ArrowLeft.pressed = false;
       break;
   }
+
   console.log(event.key);
 });
