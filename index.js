adaaff485ebb6d58e77deb8c1bbaa56aeb1415af
rhazama <rhazama01@gmail.com>
2022-08-12 19:06:10 -0700

    added rectangularCollision Fucntion

--- index.js
+++ index.js
@@ -114,6 +114,15 @@ const keys = {
 };
 let lastKey;
 
+function rectangularCollision({ rectangle1, rectangle2 }) {
+  return (
+    rectangle1.attackBox.position.x + rectangle1.attackBox.width >= rectangle2.position.x &&
+    rectangle1.attackBox.position.x <= rectangle2.position.x + rectangle2.width &&
+    rectangle1.attackBox.position.y + rectangle1.attackBox.height >= rectangle2.position.y &&
+    rectangle1.attackBox.position.y <= rectangle2.position.y + rectangle2.height
+  )
+}
+
 function animate() {
   window.requestAnimationFrame(animate);
   c.fillStyle = "black";
