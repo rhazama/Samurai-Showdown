6f79d5571fb21f7524944bc4dcd2b869cf0ecdc7
rhazama <rhazama01@gmail.com>
2022-08-19 18:03:28 -0700

    adding offset for attack

--- js/classes.js
+++ js/classes.js
@@ -61,6 +61,7 @@ class Fighter extends Sprite {
     framesMax = 1,
     offset = { x: 0, y: 0 },
     sprites,
+    attackBox = { offset: {}, width: undefined, height: undefined }
   }) {
     super({
       position,
@@ -102,7 +103,14 @@ class Fighter extends Sprite {
     this.animateFrames();
 
     this.attackBox.position.x = this.position.x + this.attackBox.offset.x;
-    this.attackBox.position.y = this.position.y + this.attackBox.offset.y;
+    this.attackBox.position.y = this.position.y;
+
+    c.fillRect(
+      this.attackBox.position.x,
+      this.attackBox.position.y,
+      this.attackBox.width,
+      this.attackBox.height
+    );
 
     this.position.x += this.velocity.x;
     this.position.y += this.velocity.y;
