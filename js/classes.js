fb0cd36f8679821fcbfdc494cefe4168cdca3845
rhazama <rhazama01@gmail.com>
2022-08-22 15:21:27 -0700

    overriding when fighter gets hit

--- js/classes.js
+++ js/classes.js
@@ -137,12 +137,17 @@ class Fighter extends Sprite {
   switchSprite(sprite) {
     //overriding all other animations with the attack animation
     if (
-      this.image === this.sprites.idle.image &&
+      this.image === this.sprites.attack1.image &&
       this.framesCurrent < this.sprites.attack1.framesMax - 1
     )
       return;
 
     // override when fighter gets hit
+    if (
+        this.image === this.sprites.takeHit.image &&
+        this.framesCurrent < this.sprites.takeHit.framesMax - 1
+      )
+        return;
 
     switch (sprite) {
       case "idle":
