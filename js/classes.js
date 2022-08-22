86f885192ffb0bd2b8592e2cf8c7b0ba8d884874
rhazama <rhazama01@gmail.com>
2022-08-22 15:13:18 -0700

    added get hit to classes

--- js/classes.js
+++ js/classes.js
@@ -172,6 +172,13 @@ class Fighter extends Sprite {
           this.framesCurrent = 0;
         }
         break;
+        case "takeHit":
+        if (this.image !== this.sprites.takeHit.image) {
+          this.image = this.sprites.takeHit.image;
+          this.framesMax = this.sprites.takeHit.framesMax;
+          this.framesCurrent = 0;
+        }
+        break;
     }
   }
 }
